6b325fc7f63f571aafa2a12b7b871f0dfdccc597
IkramCode <moikramcoder@gmail.com>
2024-07-06 03:42:23 +0500

    writing utility codes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import dotenv from "dotenv";
 import connectDB from "./db/database.js";
 
 dotenv.config({
-    path : './env'
-})
+  path: "./env",
+});
 
-connectDB()   
+connectDB()
+  .then(() => {
+
+    app.on('error' , (error) => {
+        console.log("Error :" , error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000) , () => {
+        console.log(`App is listening at ${process.env.PORT}`);
+    }
+  })
+  .catch((error) => console.log(error));
