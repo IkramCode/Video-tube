707531ea7412188132ca0ca6c1aa343a17520420
IkramCode <moikramcoder@gmail.com>
2024-07-09 16:25:25 +0500

    setup is completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/database.js";
+import {app} from './app.js'
 
 dotenv.config({
   path: "./env",
@@ -7,14 +8,13 @@ dotenv.config({
 
 connectDB()
   .then(() => {
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
 
-    app.on('error' , (error) => {
-        console.log("Error :" , error);
-        throw error
-    })
-
-    app.listen(process.env.PORT || 8000) , () => {
-        console.log(`App is listening at ${process.env.PORT}`);
-    }
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listening at ${process.env.PORT}`);
+    });
   })
   .catch((error) => console.log(error));
