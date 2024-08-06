467cedf0ceb659da62f7c453e8df7e03222c4137
IkramCode <moikramcoder@gmail.com>
2024-08-06 14:35:48 +0500

    add and debug dashboard-controller using postman

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -24,6 +24,7 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import healthcheckRouter from "./routes/healthcheck.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 app.use("/api/v1/users", userRouter);
@@ -33,6 +34,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 export { app };
